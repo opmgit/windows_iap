3c625885fc389ea43cd77c9a5d07c2c715a77287
tbm98 <tbm98.dev@gmail.com>
2022-07-26 13:50:02 +0700

    add getExpiratonDate to get real DateTime expiration of StoreLicense

--- lib/models/store_license.dart
+++ lib/models/store_license.dart
@@ -11,6 +11,24 @@ class StoreLicense {
     this.expirationDate,
   });
 
+  /// get Expiration date in DateTime format.
+  DateTime? getExpirationDate() {
+    if (expirationDate == null) {
+      return null;
+    }
+
+    var magic = num.tryParse(expirationDate.toString().substring(0, 14));
+    if (magic == null) {
+      return null;
+    }
+    // 11644499200000 is the count of millis from DateTime(1601,1,1) -> DateTime(1970,1,1)
+    // more docs: https://docs.microsoft.com/en-us/uwp/cpp-ref-for-winrt/clock
+    // https://docs.microsoft.com/en-us/uwp/api/windows.foundation.datetime?view=winrt-22621#remarks
+    // https://docs.microsoft.com/en-us/uwp/api/windows.services.store.storelicense.expirationdate?view=winrt-22621
+    magic = magic - 11644499200000;
+    return DateTime.fromMillisecondsSinceEpoch(magic.toInt());
+  }
+
   factory StoreLicense.fromJson(Map<String, dynamic> json) {
     return StoreLicense(
         isActive: json['isActive'],
